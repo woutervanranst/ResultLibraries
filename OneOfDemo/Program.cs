9aa9cdbf069a8eb0c8a668264db80fd341c0337d
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-02-10 10:55:36 +0100

    Update Program.cs

--- OneOfDemo/Program.cs
+++ OneOfDemo/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OneOf;
+
+var result = OneOf<Success, Error>.FromT0(new Success("Hello World"));
+result.Switch(
+    success => Console.WriteLine($"Success: {success.Message}"),
+    error => Console.WriteLine($"Error: {error.Message}")
+);
+
+public record Success(string Message);
+public record Error(string Message);
